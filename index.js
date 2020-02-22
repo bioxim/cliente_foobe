89ebdcd49c823aa27a077fe5c08b7203df667c42
Ximena Camino <contact@foobe.com.ar>
2020-02-22 13:56:33 -0300

    22/2/2020

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use(cors(corsOptions));
 // Rutas de la App
 app.use('/', routes());
 
-const host = process.env.HOST || '157.245.242.28';
+const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 // iniciar app
