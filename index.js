b5bbf4ac34bd4e2cd8f1f57528bee440ecd636b4
Ximena Camino <contact@foobe.com.ar>
2020-02-28 11:16:50 -0300

    merge

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Definir un dominio(s) para recibir las peticiones
-const whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL, process.env.CLIENTE, process.env.API];
+const whitelist = ['http://167.172.151.137:5000'];
 const corsOptions = {
 	origin: (origin, callback) => {
 		console.log(origin);
