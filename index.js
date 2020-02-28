79e2922faa163953fdd39650a1c7fbb7a4b171a8
Ximena Camino <contact@foobe.com.ar>
2020-02-28 16:05:18 -0300

    bd

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(cors(corsOptions));
 app.use('/', routes());
 
 const host = '167.172.151.137';
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.get('/', (req, res) => res.send('Esta es la api de foobe.com.ar'));
 
