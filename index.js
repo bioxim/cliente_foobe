847bd01465732981b2728ab22c7ac1061eaf5304
Ximena Camino <contact@foobe.com.ar>
2020-03-02 12:49:59 -0300

    added whitepoint

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Definir un dominio(s) para recibir las peticiones
-const whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL, process.env.CLIENTE, process.env.FRONTEND];
+const whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL, process.env.CLIENTE, process.env.FRONTEND, process.env.BACKEND];
 const corsOptions = {
 	origin: (origin, callback) => {
 		//console.log(origin);
