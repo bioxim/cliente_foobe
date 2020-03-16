9c0c2c34485fa816e71c5df700057474c0e9d30f
Ximena Camino <contact@foobe.com.ar>
2020-03-15 21:05:53 -0300

    hol

--- index.js
+++ index.js
@@ -54,12 +54,12 @@ app.use(cors(corsOptions));
 // Rutas de la App
 app.use('/', routes());
 
-//const host = process.env.HOST || '104.248.48.153';
-//const port = 5000;
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Esta es la api de foobe.com.ar'));
 
 // iniciar app
-app.listen(5000, () => {
-	console.log('el servidor está funcionando en el puerto:', 5000);
+app.listen(port, host, () => {
+	console.log('el servidor está funcionando en el puerto:', port);
 });
