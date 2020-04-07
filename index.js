c11845423faebce6fd22ee19d36df8fc30e19750
Ximena Camino <contact@foobe.com.ar>
2020-04-07 19:32:56 -0300

    sincors

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 require ('dotenv').config({ path: 'variables.env'});
 
-// Cors permite que un cliente se conecte a otro servidor para el intercambio de recursos
-//const cors = require('cors');
-
 // conectar mongo
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB_URL, {
@@ -33,22 +30,6 @@ app.use(expressValidator());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Definir un dominio(s) para recibir las peticiones
-//const whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL, process.env.FRONTEND, process.env.CLIENTE, process.env.SERVIDORLOCAL, process.env.API];
-//console.log(whitelist);
-/*const corsOptions = {
-	origin: (origin, callback) => {
-		//console.log(origin);
-		// Revisar si la petición viene de un servidor que está en la lista whitelist
-		const existe = whitelist.some( dominio => dominio === origin );
-		if(existe) {
-			callback(null, true);
-		} else {
-			callback(new Error('No permitido por CORS'));
-		}
-	}
-}*/
-
 // Habilitar cors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -58,12 +39,9 @@ app.use((req, res, next) => {
     next();
 });
 
-//app.use(cors(corsOptions));
-
 // Rutas de la App
 app.use('/', routes());
 
-//const host = 'localhost';
 const port = 5000;
 
 app.get('/', (req, res) => res.send('Esta es la api de foobe.com.ar'));
